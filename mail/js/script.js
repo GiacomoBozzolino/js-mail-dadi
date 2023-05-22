29141d0ec9ff5bb32a7bdc07a4935824992a1411
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-22 16:27:42 +0200

    [FEAT] aggiunta verifica con flag

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,15 +1,22 @@
-let email = ['gandalf@gmail.com', 'frodo@gmail.com', 'sam@gmail.com', 'aragorn@gmail.com', 'legolas@gmail.com', 'gimli@gmail.com', 'merry@gmail.com', 'pipino@gmail.com', 'boromir@gmail.com'];
+"use strict";
 
+let email_adress = ['gandalf@gmail.com', 'frodo@gmail.com', 'sam@gmail.com', 'aragorn@gmail.com', 'legolas@gmail.com', 'gimli@gmail.com', 'merry@gmail.com', 'pipino@gmail.com', 'boromir@gmail.com'];
 
+// console.log (email_adress)
 
-let search = prompt ("Inserisci l'indirzzo email che vuoi cercare");
+let email = prompt ("Inserisci l'indirzzo email che vuoi cercare");
+let flag = false
 
-for (let i=0; i<email.length; i++){
-    if (email[1]=== search){
-        console.log ('Email trovata!');
-    } else{ 
-        console.log ('Email non trovata!');
-    }
+for (let i=0; i<email_adress.length; i++){  
 
-    
+    if (email_adress[i] === email) {
+    //    console.log('email trovata')  
+       flag === true;
+    } 
+}
+
+if (flag === true) {
+    console.log ('email trovata');
+} else {
+    console.log ('email non trovata');
 }
